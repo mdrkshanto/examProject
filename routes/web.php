112637003b52e7cc0961a5e65b9c692cc4ffa22a
Md Rezaul Karim Shanto <bdshantogithub@outlook.com>
2025-05-02 11:07:01 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExamController;
 
-Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('login', [AuthController::class, 'login'])->name('post.login');
 Route::middleware('auth')->as('exam.')->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('exam-list', [ExamController::class, 'index'])->name('list');
+    Route::get('/', [ExamController::class, 'index'])->name('list');
     Route::get('exam-detail/{id}', [ExamController::class, 'detail'])->name('detail');
     Route::get('exam-start/{exam}/{question}', [ExamController::class, 'start'])->name('start');
     Route::post('exam-submit/{question}',[ExamController::class,'submit'])->name('submit');
